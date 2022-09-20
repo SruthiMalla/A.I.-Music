6746c8561324028e5c164c9c97251796aba3365f
SruthiMalla <52434141+SruthiMalla@users.noreply.github.com>
2022-09-20 16:47:12 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 song1 = "";
 song2 = "";
+leftwristX = 0;
+leftwristY = 0;
+rightwristX = 0;
+rightwristY = 0;
 
 function preload()
 {
@@ -13,9 +17,33 @@ function setup()
     canvas.center();
     video = createCapture(VIDEO);
     video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
 }
 
 function draw()
 {
     image(video, 0, 0, 600, 500);
+}
+
+function modelLoaded()
+{
+    console.log("poseNet is initialized");
+}
+
+function gotPoses(results)
+{
+    if(results.length > 0)
+    {
+        console.log(results);
+
+        leftwristX = results[0].pose.leftWrist.x;
+        leftwristY = results[0].pose.leftWrist.y;
+        console.log("leftwristX = " + leftwristX + "leftwristY = " + leftwristY);
+
+        rightwristX = results[0].pose.rightWrist.x;
+        rightwristY = results[0].pose.rightWrist.y;
+        console.log("rightwristX = " + rightwristX + "rightwristY = " + rightwristY);
+    }
 }
